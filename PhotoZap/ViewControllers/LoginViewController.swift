74428b140f64225ce36909a974ddce6ba4c62eec
Kimberly Zai <kimberlyz621@gmail.com>
2016-01-14 23:03:35 -0800

    Starting to create custom login page.

---
+++ PhotoZap/ViewControllers/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  PhotoZap
+//
+//  Created by Kimberly Zai on 1/14/16.
+//  Copyright (c) 2016 Kimberly Zai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        
+    }
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        
+    }
+}
